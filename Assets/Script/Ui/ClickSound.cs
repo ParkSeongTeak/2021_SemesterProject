c14b35b0292841fc57a577b2478122313bb4042c
blueskyEunhan <76895039+blueskyEunhan@users.noreply.github.com>
2021-05-03 16:14:31 +0900

    sound
    sound

---
+++ Assets/Script/Ui/ClickSound.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickSound : MonoBehaviour
+{
+    public AudioSource Fx;
+    public AudioClip click;
+
+    public void Click()
+    {
+        Fx.PlayOneShot(click);
+    }
+}
