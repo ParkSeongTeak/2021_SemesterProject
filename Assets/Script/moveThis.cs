2ebdc1980d464dcd7e30a9455c5d8f04631ff088
ParkSeongtaek <56757197+ParkSeongtaek@users.noreply.github.com>
2021-05-13 11:33:44 +0900

    박성택

---
+++ Assets/Script/moveThis.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class moveThis : MonoBehaviour
+{
+    void Start()
+    {
+        transform.position = new Vector3(0, 0, 0);
+        Debug.Log(transform.position.x);
+    }
+}
