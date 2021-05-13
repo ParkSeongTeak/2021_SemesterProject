cda0fd31ecca84f7fd4e28a8097f2d566377c001
ParkSeongtaek <56757197+ParkSeongtaek@users.noreply.github.com>
2021-05-13 15:09:07 +0900

    박성택

--- Assets/Script/Collision/Controllable_Collision.cs
+++ Assets/Script/Collision/Controllable_Collision.cs
@@ -7,47 +7,50 @@ public class Controllable_Collision : MonoBehaviour
 {
     public void OnCollisionEnter2D(Collision2D col)
     {
-        //피버상태가 아닐때의 함수들입니다.
-        if (GameManager.instance.Is_Fever == false)
+        if (this.gameObject.tag == "Controllable")
         {
-            //충돌체의 태그가 First일때, 각 태그가 First, Second로 바뀌며 점수를 획득합니다.
-            if (col.gameObject.tag == "First")
+            //피버상태가 아닐때의 함수들입니다.
+            if (GameManager.instance.Is_Fever == false)
             {
-                col.gameObject.tag = "Second";
-                this.gameObject.tag = "First";
-                GameManager.instance.Get_Point++;
+                //충돌체의 태그가 First일때, 각 태그가 First, Second로 바뀌며 점수를 획득합니다.
+                if (col.gameObject.tag == "First")
+                {
+                    col.gameObject.tag = "Second";
+                    this.gameObject.tag = "First";
+                    GameManager.instance.Get_Point++;
+                }
+
+                //충돌체의 태그가 Second일때,  각 태그가 First, Second로 바뀌며 점수를 획득하지 않습니다.
+                else if (col.gameObject.tag == "Second")
+                {
+                    col.gameObject.tag = "Second";
+                    this.gameObject.tag = "First";
+                }
+
+                //side tag를 가진 물체와 충돌할 시 게임오버값을 true로 바꿉니다.
+                else if (col.gameObject.tag == "Side")
+                {
+                    GameManager.instance.GameOver = true;
+                }
             }
 
-            //충돌체의 태그가 Second일때,  각 태그가 First, Second로 바뀌며 점수를 획득하지 않습니다.
-            else if (col.gameObject.tag == "Second")
+            //피버상태일때의 함수입니다.
+            else
             {
-                col.gameObject.tag = "Second";
-                this.gameObject.tag = "First";
-            }
+                //충돌체의 태그가 First일때, 각 태그가 First, Second로 바뀌며 점수를 획득합니다.
+                if (col.gameObject.tag == "First")
+                {
+                    col.gameObject.tag = "Second";
+                    this.gameObject.tag = "First";
+                    GameManager.instance.Get_Point++;
+                }
 
-            //side tag를 가진 물체와 충돌할 시 게임오버값을 true로 바꿉니다.
-            else if (col.gameObject.tag == "Side")
-            {
-                GameManager.instance.GameOver = true;
-            }
-        }
-
-        //피버상태일때의 함수입니다.
-        else
-        {
-            //충돌체의 태그가 First일때, 각 태그가 First, Second로 바뀌며 점수를 획득합니다.
-            if (col.gameObject.tag == "First")
-            {
-                col.gameObject.tag = "Second";
-                this.gameObject.tag = "First";
-                GameManager.instance.Get_Point++;
-            }
-
-            //충돌체의 태그가 Second일때,  각 태그가 First, Second로 바뀌며 점수를 획득하지 않습니다.
-            else if (col.gameObject.tag == "Second")
-            {
-                col.gameObject.tag = "Second";
-                this.gameObject.tag = "First";
+                //충돌체의 태그가 Second일때,  각 태그가 First, Second로 바뀌며 점수를 획득하지 않습니다.
+                else if (col.gameObject.tag == "Second")
+                {
+                    col.gameObject.tag = "Second";
+                    this.gameObject.tag = "First";
+                }
             }
         }
     }
--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -2,15 +2,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-//박성택의 class
-[System.Serializable]
-public class SeongTaek
-{
-    string str = "타인의 코드와 자기 코드가 섞이지 않도록 조심합시다";
-}
-
-
-
 
 
 public class GameManager : MonoBehaviour        //이 클래스는 public 하게 설정되었습니다. 다른 script에서도 이 class를 사용 할 수 있습니다.  
@@ -21,32 +12,15 @@ public class GameManager : MonoBehaviour        //이 클래스는 public 하게
 
 
     public static GameManager instance;         //이 클래스의 객체입니다. 우리는 이제부터 이 객체를 게임 매니저라고 생각하고 사용할 것입니다.
-    public int Publicnum = 0;                   //예를 들어 other script에서 이 num의 값에 1을 더하고 싶다면 GameManager.instance.Publicnum += 1; 이라고 쓰면 게임 매니저 instance 객체의 num값에 1이 더해질 것입니다
-
+    
     public bool GameOver = false;               //게임이 오버되었는지 알아보는 부울값입니다.
     public int Get_Point = 0;                   //게임 실행할 때 나타날 포인트입니다.
     public bool Is_Fever = false;               //피버상태인지 알아보는 부울값입니다.
     public int Twins_Count = 0;                 //Twins 큐브가 1번 쌓일때마다 1씩 증가하며 count가 2가 되면 2포인트를 획득합니다.
     public bool IsFuncTwice = false;
+    
+    
 
-    string PrefsPublicnum = "저장하기 위한 string입니다 아래SaveData()에서 어떻게 쓰는지 봅시다";
-
-
-
-    private int Privatenum = 0;                 //훠어얼씬 안전하고 좋은 private 변수입니다. private 이 public 보다 좋은 이유는 30개가 넘고 public 이 private보다 좋은 이유는 "편하다" 단 한 가지뿐입니다.
-                                                //그래서 저는 public을 주로 씁니다....... 뭐 그냥 private이 더 좋다는건 알아둡시다.
-
-    string PrefsPrivatenum = "저장을 위한 string은 절때 public하게 만들지맙시다...그걸로 3일간 삽질해봤습니다....";
-
-    public void setPrivatenum(int n)            //함수 위에 참조 개수도 표시가 되어있습니다. ?? 안 보이는데요?? 위  GameManager도 청녹색으로 안보이고 그냥 흰색인데요? 하면 그 비주얼 스튜디오 Unity뭐시기가 안 깔린 겁니다  
-                                                //그걸 어디서 깔더라.... 아무튼 문의주세요 기억은 잘 안 납니다만 같이 찾아보죠...... 참조 글씨가 있으면 마우스 올리고 어디서 참조 중인지를 한번 봐봅시다 역시 private이 좋긴 합니다.......
-    {
-        Privatenum = n;
-    }                                           //Privatenum에 수를 넣는 public 함수 other script에서 접근 가능 GameManager.instance.setPrivatenum(n);
-    public int getPrivatenum()             
-    {                                           
-        return Privatenum;
-    }                                           //Privatenum의 수를 가져오는 public 함수 other script에서 접근가능 GameManager.instance.getPrivatenum();
 
 
     private void Awake()                        //Scene 이동을 하더라도 이 객체는 부서지지 않습니다. Scene 이동을 통해 어떤 식으로 객체가 부서지지 않는지 확인해봅시다. 
@@ -61,13 +35,9 @@ public class GameManager : MonoBehaviour        //이 클래스는 public 하게
 
     public void SaveData()                                      //변수 저장 방법입니다.
     {                                                           //other script에서 불러봅시다.
-        PlayerPrefs.SetInt(PrefsPublicnum, Publicnum);          //PlayerPrefs.SetInt( 암호화용string , 저장할 숫자);         //저 문자열에 숫자가 저장되었습니다
-        PlayerPrefs.SetInt(PrefsPrivatenum, Privatenum);        //PlayerPrefs.SetInt( 암호화용string , 저장할 숫자); 
     }
     public void GetDate()                                       //저장한 변수를 불러오는 방법입니다.
     {
-        Publicnum = PlayerPrefs.GetInt(PrefsPublicnum,0);       //문자열에 저장된 숫자를 가지고옵니다 (Set/Get)Float , (Set/Get)String도 가능합니다
-        Privatenum = PlayerPrefs.GetInt(PrefsPrivatenum, 0);    //만약 해당 문자열로 Set한적이 없다면 오른쪽 0을 가져옵니다.
     }
 
 }
