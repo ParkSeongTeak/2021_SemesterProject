e5f3b9e127fc41f4e78283735583f531ab2b332c
ParkSeongtaek <56757197+ParkSeongtaek@users.noreply.github.com>
2021-06-29 01:18:01 +0900

    박성택

--- Assets/Script/Ui/cameraMoving.cs
+++ Assets/Script/Ui/cameraMoving.cs
@@ -10,7 +10,7 @@ public class cameraMoving : MonoBehaviour
 
     private void Update()
     {
-        if (GameManager.instance.firstHeight >= 2 + maincamera.transform.position.y) //일단 임의의 숫자 넣음 -> 추후 192로 변경
+        if (GameManager.instance.firstHeight >= 6 + maincamera.transform.position.y) //일단 임의의 숫자 넣음 -> 추후 192로 변경
         {
             Vector3 cubePos = new Vector3(this.transform.position.x, GameManager.instance.firstHeight + 3f, this.transform.position.z); //역시 임의의 숫자 -> 추후 480으로 변경
             transform.position = Vector3.Lerp(transform.position, cubePos, Time.deltaTime * movingspeed);
