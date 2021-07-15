d37082ad977b2f785d30cd4e34581afbda852b8b
ParkSeongtaek <56757197+ParkSeongtaek@users.noreply.github.com>
2021-07-15 22:18:54 +0900

    박성택

---
+++ Assets/Our_Asset/Script/fever.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fever : MonoBehaviour
+{
+    int i = 0;
+    float time = 0.25f;
+    float timer = 0.0f;
+
+    public GameObject[] Fev = new GameObject[5];
+    // Start is called before the first frame update
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= time) {
+            timer = 0.0f;
+
+            for (int j = 0; j < 5; j++)
+                Fev[j].SetActive(false);
+
+            Fev[i].SetActive(true);
+            i++;
+            if (i == 5)
+                i = 0;
+        }
+
+    }
+}
---
+++ Assets/Our_Asset/Script/fever2.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fever2 : MonoBehaviour
+{
+    int i = 0;
+    float time = 0.5f;
+    float timer = 0.5f;
+
+    public GameObject[] Fev = new GameObject[5];
+    // Start is called before the first frame update
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= time)
+        {
+            timer = 0.0f;
+
+            for (int j = 0; j < 5; j++)
+                Fev[j].SetActive(false);
+
+            Fev[i].SetActive(true);
+            i++;
+            if (i == 5)
+                i = 0;
+        }
+
+    }
+}
